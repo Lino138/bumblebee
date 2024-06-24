5d9d9d5df6dd31505f2b7aa873ad0771dc19b056
Brian Michelson <brian.michelson@uni-freiburg.de>
2024-06-24 11:59:15 +0200

    s

--- researcher_workspace/urls.py
+++ researcher_workspace/urls.py
@@ -69,7 +69,8 @@ else:
 
 
 urlpatterns = [
-    path('', views.index, name='index'),  
+    path('admin/', admin.site.urls),
+    path('', include('researcher_workspace.urls')),
     path('home/', views.home, name='home'),
     path('select_vm/', views.home, name='select_vm'),  # Handle form submission
     path('desktop/<str:desktop_name>/', views.desktop_details, name='desktop_details'),
